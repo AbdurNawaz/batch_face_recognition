ec94ff9ecde6b1be4bff28f5b6306fcbb8722ddf
Abdur Nawaz <abdurnawaz.2011@gmail.com>
2024-06-09 14:25:50 +0530

    added batch face encodings method

--- face_recognition/api.py
+++ face_recognition/api.py
@@ -214,6 +214,24 @@ def face_encodings(face_image, known_face_locations=None, num_jitters=1, model="
     return [np.array(face_encoder.compute_face_descriptor(face_image, raw_landmark_set, num_jitters)) for raw_landmark_set in raw_landmarks]
 
 
+def batch_face_encodings(face_image, known_face_locations=None, num_jitters=1, model="small"):
+    """
+    Given an image, return the 128-dimension face encoding for each face in the image.
+
+    Calculates the encodings in a batch
+
+    :param face_image: The image that contains one or more faces
+    :param known_face_locations: Optional - the bounding boxes of each face if you already know them.
+    :param num_jitters: How many times to re-sample the face when calculating encoding. Higher is more accurate, but slower (i.e. 100 is 100x slower)
+    :param model: Optional - which model to use. "large" (default) or "small" which only returns 5 points but is faster.
+    :return: A list of 128-dimensional face encodings (one for each face in the image)
+    """
+    dlib_vector = dlib.full_object_detections()
+    raw_landmarks = _raw_face_landmarks(face_image, known_face_locations, model)
+    dlib_vector.extend(raw_landmarks)
+    return np.array(face_encoder.compute_face_descriptor(face_image, dlib_vector, num_jitters))
+
+
 def compare_faces(known_face_encodings, face_encoding_to_check, tolerance=0.6):
     """
     Compare a list of face encodings against a candidate encoding to see if they match.
